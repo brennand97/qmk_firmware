a46cba00b2b64f536dacbaf7b99116501e3fb28e
Brennan Douglas <brennand97@gmail.com>
2020-07-28 22:19:56 -0700

    Some layout modifications + add the led_timeout module

--- keyboards/massdrop/ctrl/keymaps/brennand97/config.h
+++ keyboards/massdrop/ctrl/keymaps/brennand97/config.h
@@ -3,10 +3,10 @@
 // #define RGBLIGHT_HUE_STEP 12                                     // Units to step when in/decreasing hue
 // #define RGBLIGHT_SAT_STEP 25                                     // Units to step when in/decreasing saturation
 // #define RGBLIGHT_VAL_STEP 12                                     // Units to step when in/decreasing value (brightness)
-//#define RGB_MATRIX_SPD_STEP 16                                    // Units to step when in/decreasing speed
+// #define RGB_MATRIX_SPD_STEP 16                                   // Units to step when in/decreasing speed
 
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_BAND_VAL                 // Sets default animation
 #define RGB_MATRIX_STARTUP_SPD 31                                   // Sets the default animation speed
-#define RGB_MATRIX_STARTUP_HUE 32                                   // Sets the default hue value
+#define RGB_MATRIX_STARTUP_HUE 6                                    // Sets the default hue value
 #define RGB_MATRIX_STARTUP_SAT 255                                  // Sets the default saturation value
 #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS        // Sets the default brightness value
--- keyboards/massdrop/ctrl/keymaps/brennand97/keymap.h
+++ keyboards/massdrop/ctrl/keymaps/brennand97/keymap.h
@@ -1,12 +1,17 @@
 #include QMK_KEYBOARD_H
 
+#include "led_timeout.h"
+
+// Remember this is also the order of the layers!!
 enum layout_names {
     _KL=0,       // Keys Layout: The main keyboard layout that has all the characters
-    _FL,         // Function Layout: The function key activated layout with default functions and some added ones
+    _MT,         // Mac Toggle: Flips the Ctrl and windows key when the windows key is pressed (good for shortcuts while keeping the windows key functional)
     _NL,         // Numpad Layout: Adds a 10-key number pad to the center of the board
+    _FL,         // Function Layout: The function key activated layout with default functions and some added ones
 };
 
-enum ctrl_keycodes {
+enum custom_keycodes {
+    // ctrl keycodes
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
     U_T_AGCR,              //USB Toggle Automatic GCR control
     DBG_TOG,               //DEBUG Toggle On / Off
@@ -14,8 +19,12 @@ enum ctrl_keycodes {
     DBG_KBD,               //DEBUG Toggle Keyboard Prints
     DBG_MOU,               //DEBUG Toggle Mouse Prints
     MD_BOOT,               //Restart into bootloader after hold timeout
+
+    // led ctrl keycodes
+    ROUT_TG,               // Timeout Toggle. Toggle idle LED time out on or off
+    ROUT_VI,               // Timeout Value Increase. Increase idle time out before LED disabled
+    ROUT_VD,               // Timeout Value Decrease. Decrease idle time out before LED disabled
+    ROUT_FM,               // RGB timeout fast mode toggle
 };
 
-// enum custom_keycodes {
-//     TG_MAC = MD_BOOT + 1 // This should always be the last enum from ctrl_keycodes + 1
-// }
+#define MT_MAC MT(MOD_LCTL, KC_LGUI)
